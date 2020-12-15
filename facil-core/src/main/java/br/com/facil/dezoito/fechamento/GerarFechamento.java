bf636b9e070ba2625cdf1e1192144199b27f5922
Santunis Monteiro de Oliveira <santunis.oliveira@fornecedores.sicoob.com.br>
2020-12-14 22:21:30 -0300

    Criando Fechamento

---
+++ facil-core/src/main/java/br/com/facil/dezoito/fechamento/GerarFechamento.java
@@ -0,0 +1,56 @@
+package br.com.facil.dezoito.fechamento;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import br.com.facil.Combinacoes;
+import br.com.facil.excecao.LotoException;
+
+public class GerarFechamento {
+
+	public static void main(String[] args) throws IOException, LotoException {
+
+		String[] str = { 
+/*			|#########################################|
+/*			|*/		"1", "2", "3", /*"4",*/ "5",        //|
+/*			|#########################################|
+/*			|*/		"6", "7", /*"8",*/ /*"9",*/ "10",	//|
+/*			|#########################################|
+/*			|*/		"11", /*"12",*/ "13", "14", "15",	//|
+/*			|#########################################|
+/*			|*/		"16", /*"17",*/ "18", "19", /*"20",*/	//|
+/*			|#########################################|
+/*			|*/		"21", "22", "23", "24", /*"25"*/	//|
+//			|#########################################|
+		};
+
+		FileWriter arq = new FileWriter("D:\\projetos_loto\\18_25\\facil-core\\src\\main\\resources\\fechamento\\GerarFechamento18.csv");
+		PrintWriter gravarArq = new PrintWriter(arq);
+		
+		String[] saida;
+		long cont = 0;
+		Combinacoes comb1 = new Combinacoes(str, 15);
+		while (comb1.hasNext()) {
+			String lista = null;
+			saida = comb1.next();
+			
+			lista = saida[0] + "," + saida[1] + "," + saida[2] + "," + saida[3] + "," + saida[4] + "," + 
+					saida[5] + "," + saida[6] + "," + saida[7] + "," + saida[8] + "," + saida[9] + "," + 
+					saida[10] + "," + saida[11] + "," + saida[12] + "," + saida[13] + "," + saida[14];
+			
+			gravarArq.printf("%s%n", lista);
+			
+			cont++;
+			System.out.println(cont + " - " + 
+					saida[0] + "," + saida[1] + "," + saida[2] + "," + saida[3] + "," + saida[4] + "," + 
+					saida[5] + "," + saida[6] + "," + saida[7] + "," + saida[8] + "," + saida[9] + "," + 
+					saida[10] + "," + saida[11] + "," + saida[12] + "," + saida[13] + "," + saida[14]
+			);
+		}
+		
+		arq.close();
+		gravarArq.close();
+	}
+
+}
